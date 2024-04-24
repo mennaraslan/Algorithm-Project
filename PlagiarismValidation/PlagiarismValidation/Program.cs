4d29b1cf0397963a165a1ef40780eba2d9396712
Roner8iny <ronymohamed2323@gmail.com>
2024-04-24 21:26:15 +0200

    Modified Returned Tuple

--- PlagiarismValidation/PlagiarismValidation/Program.cs
+++ PlagiarismValidation/PlagiarismValidation/Program.cs
@@ -15,7 +15,7 @@ namespace PlagiarismValidation
         static void Main(string[] args)
         {
 
-            Tuple<string, string, float, int>[] edges = ReadFromExcelFile();
+            Tuple<string, string, int, int, int>[] edges = ReadFromExcelFile();
             //var edges = new Tuple<string, string, float>[numberOfedges];
 
             foreach (var edge in edges)
@@ -32,12 +32,12 @@ namespace PlagiarismValidation
 
         }
 
-        public static Tuple<string, string, float, int>[] ReadFromExcelFile()
+        public static Tuple<string, string, int, int, int>[] ReadFromExcelFile()
         {
             string inputfilePath = "D:\\Uni Related\\Algorithms\\Project\\MATERIALS\\[3] Plagiarism Validation\\Algorithm-Project\\PlagiarismValidation\\trial input.xlsx";
             int numberOfEdges;
             //var edges = new Tuple<string, string, float>[numberOfedges];
-            Tuple<string, string, float, int>[] edges;
+            Tuple<string, string, int, int, int>[] edges;
 
             using (var stream = File.Open(inputfilePath, FileMode.Open, FileAccess.Read))
             {
@@ -53,26 +53,60 @@ namespace PlagiarismValidation
                 string column2 = "";
                 string column3 = "";
 
-                string[] similarityPercentageArr;
-                string similarityPercentage;
-                string mergedSimilarityPercentage;
-                Match matchPercentage;
-                float percentageDoc1 , percentageDoc2;
-                Regex percentageRegex = new Regex(@"\(\d+%\)");
+                //string[] similarityPercentageArr;
+                //string similarityPercentage;
+                //string mergedSimilarityPercentage;
+                //Match matchPercentage;
+                //float percentageDoc1 , percentageDoc2;
+                //Regex percentageRegex = new Regex(@"\(\d+%\)");
 
-                string[] documentPathArr;
-                string documentPath1 = "";
-                string documentPath2 = "";
-                Regex documentPathRegex = new Regex(@"~(\(\d+%\))");
-                Match matchDocumentPath;
+                //string[] documentPathArr;
+                //string documentPath1 = "";
+                //string documentPath2 = "";
+                //Regex documentPathRegex = new Regex(@"~(\(\d+%\))");
+                //Match matchDocumentPath;
 
                 int linesMatched;
 
                 numberOfEdges = table.Rows.Count - 1;
                 Console.WriteLine(numberOfEdges);
-                edges = new Tuple<string, string, float, int>[numberOfEdges];
-                float percentageToBeRecorded;
+                edges = new Tuple<string, string, int, int, int>[numberOfEdges];
+                //float percentageToBeRecorded;
 
+
+                //static Tuple<string, string, float, int>[] ExtractTableData(List<string> tableHtml, int n)
+                //{
+                //    Tuple<string, string, float, int>[] data = new Tuple<string, string, float, int>[n];
+                //    int indx = 0;
+                //    for (int i = 0; i < tableHtml.Count(); i += 3)
+                //    {
+                //        string item1 = tableHtml[i];
+                //        int indexOfbrac1 = item1.LastIndexOf('(');
+                //        string firstPath = item1.Substring(0, indexOfbrac1);
+                //        float firstPercntage = int.Parse(item1.Substring(indexOfbrac1 + 1, item1.Length - (indexOfbrac1 + 3)));
+
+                //        string item2 = tableHtml[i + 1];
+                //        int indexOfbrac2 = item2.LastIndexOf('(');
+                //        string secondPath = item2.Substring(0, indexOfbrac2);
+                //        float secondPercntage = int.Parse(item2.Substring(indexOfbrac2 + 1, item2.Length - (indexOfbrac2 + 3)));
+
+                //        int linesMatched = int.Parse(tableHtml[i + 2]);
+
+                //        float finalPercentage = 0;
+                //        if (firstPercntage >= secondPercntage)
+                //        {
+                //            finalPercentage = firstPercntage;
+                //        }
+                //        else
+                //        {
+                //            finalPercentage = secondPercntage;
+                //        }
+
+                //        data[indx] = Tuple.Create(firstPath, secondPath, finalPercentage, linesMatched);
+                //        indx++;
+                //    }
+                //    return data;
+                //}
                 for (int i = 1; i < table.Rows.Count; i++)
                 {
                         
@@ -81,35 +115,41 @@ namespace PlagiarismValidation
                     column1 = row[0].ToString();
 
                     // Retrieving the similarity percentage of each document in column 1
-                    matchPercentage = percentageRegex.Match(column1); // Output --> (percentage%)
-                    similarityPercentage = matchPercentage.Value;
-                    similarityPercentageArr = similarityPercentage.Split('(', '%');
-                    mergedSimilarityPercentage = string.Join("", similarityPercentageArr);
-                    mergedSimilarityPercentage = mergedSimilarityPercentage.Replace(")", "");
-                    mergedSimilarityPercentage = Regex.Replace(mergedSimilarityPercentage, @"\D", "");
+                    //matchPercentage = percentageRegex.Match(column1); // Output --> (percentage%)
+                    int indexOfbrac1 = column1.LastIndexOf('(');
+                    string firstPath = column1.Substring(0, indexOfbrac1);
+                    int firstPercntage = int.Parse(column1.Substring(indexOfbrac1 + 1, column1.Length - (indexOfbrac1 + 3)));
+                    //similarityPercentage = matchPercentage.Value;
+                    //similarityPercentageArr = similarityPercentage.Split('(', '%');
+                    //mergedSimilarityPercentage = string.Join("", similarityPercentageArr);
+                    //mergedSimilarityPercentage = mergedSimilarityPercentage.Replace(")", "");
+                    //mergedSimilarityPercentage = Regex.Replace(mergedSimilarityPercentage, @"\D", "");
                     //percentageDoc1 = Convert.ToInt32(mergedSimilarityPercentage);
-                    percentageDoc1 = float.Parse(mergedSimilarityPercentage);
+                    //percentageDoc1 = float.Parse(mergedSimilarityPercentage);
                     //Console.WriteLine(percentageDoc1);
 
                     // Retrieving the Document's Path in column 1
-                    documentPath1 = column1.Replace(similarityPercentage, "");
+                    //documentPath1 = column1.Replace(similarityPercentage, "");
                     //Console.WriteLine(documentPath1);
 
                     column2 = row[1].ToString();
 
                     // Retrieving the similarity percentage of each document in column 2
-                    matchPercentage = percentageRegex.Match(column2);
-                    similarityPercentage = matchPercentage.Value;
-                    similarityPercentageArr = similarityPercentage.Split('(', '%');
-                    mergedSimilarityPercentage = string.Join("", similarityPercentageArr);
-                    mergedSimilarityPercentage = mergedSimilarityPercentage.Replace(")", "");
-                    mergedSimilarityPercentage = Regex.Replace(mergedSimilarityPercentage, @"\D", "");
+                    //matchPercentage = percentageRegex.Match(column2);
+                    //similarityPercentage = matchPercentage.Value;
+                    //similarityPercentageArr = similarityPercentage.Split('(', '%');
+                    //mergedSimilarityPercentage = string.Join("", similarityPercentageArr);
+                    //mergedSimilarityPercentage = mergedSimilarityPercentage.Replace(")", "");
+                    //mergedSimilarityPercentage = Regex.Replace(mergedSimilarityPercentage, @"\D", "");
                     //percentageDoc2 = Convert.ToInt32(mergedSimilarityPercentage);
-                    percentageDoc2 = float.Parse(mergedSimilarityPercentage);
+                    int indexOfbrac2 = column2.LastIndexOf('(');
+                    string secondPath = column2.Substring(0, indexOfbrac2);
+                    int secondPercntage = int.Parse(column2.Substring(indexOfbrac2 + 1, column2.Length - (indexOfbrac2 + 3)));
+                    //percentageDoc2 = float.Parse(mergedSimilarityPercentage);
                     //Console.WriteLine(percentageDoc2);
 
                     // Retrieving the Document's Path in column 2
-                    documentPath2 = column2.Replace(similarityPercentage, "");
+                    //documentPath2 = column2.Replace(similarityPercentage, "");
                     //Console.WriteLine(documentPath2);
 
                     // Retrieving the Lines Matched in column 3
@@ -117,15 +157,15 @@ namespace PlagiarismValidation
                     linesMatched = Convert.ToInt32(column3);
                     //Console.WriteLine(linesMatched);
 
-                    if(percentageDoc1 >= percentageDoc2)
-                    {
-                        percentageToBeRecorded = percentageDoc1;
-                    }
-                    else
-                    {
-                        percentageToBeRecorded = percentageDoc2;
-                    }
-                    edges[i - 1] = new Tuple<string, string, float, int>(documentPath1, documentPath2, percentageToBeRecorded, linesMatched);
+                    //if(percentageDoc1 >= percentageDoc2)
+                    //{
+                    //    percentageToBeRecorded = percentageDoc1;
+                    //}
+                    //else
+                    //{
+                    //    percentageToBeRecorded = percentageDoc2;
+                    //}
+                    edges[i - 1] = new Tuple<string, string, int, int, int>(firstPath, secondPath, firstPercntage, secondPercntage, linesMatched);
                 }
 
                 if (reader != null)
